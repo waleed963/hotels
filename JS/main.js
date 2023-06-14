7fea6e5304ab7555137399e1f889a107542c5fec
waleed963 <waleedalabdallah61@gmail.com>
2023-06-14 15:56:58 +0300

    new1

---
+++ JS/main.js
@@ -0,0 +1,23 @@
+let x = parseInt(prompt("Please Enter teh x value"));
+let y = 4;
+
+document.getElementById("mydiv").innerHTML = 4 ** x;
+console.log(x + y)
+console.log(x - y)
+console.log(x * y)
+console.log(x / y)
+console.log(x % y)
+console.log(x ** y)
+
+
+
+
+
+
+
+
+
+
+
+
+//window.alert('حاجة لعب يا وليد ');
